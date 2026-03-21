df989c51362552f99bbd49ea035dfff0be45b52f
Juan Manuel <Jmmarulanda2017@hotmail.com>
2026-03-21 17:08:31 -0500

    chore: minmor updates and code cleanup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import InputField from '../components/InputField';
 import Button from '../components/Button';
 import logo from '../assets/logo.jpeg';
 import fondo from '../assets/fondo-repo.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -12,6 +13,7 @@ export default function Login() {
   const [cargando, setCargando] = useState(false);
   
   const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,6 +25,8 @@ export default function Login() {
     if (!resultado.success) {
       setError(resultado.mensaje);
       setCargando(false);
+    } else {
+      navigate('/dashboard');
     }
   };
 
